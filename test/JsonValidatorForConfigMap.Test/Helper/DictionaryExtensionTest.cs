69d83921257987017bd6d9fa879451e90d4637f5
Benjamin Krieger <bkrieger@igus.net>
2023-01-08 12:19:12 +0100

    feat: Add unit tests and created solution folders

---
+++ test/JsonValidatorForConfigMap.Test/Helper/DictionaryExtensionTest.cs
@@ -0,0 +1,47 @@
+﻿using FluentAssertions;
+using JsonValidatorForConfigMap.Helper;
+
+namespace JsonValidatorForConfigMap.Test.Helper;
+
+public class DictionaryExtensionTest
+{
+    [Theory]
+    [InlineData("Key2", "Value2")]
+    [InlineData("Nested.NestedKey2", "NestedValue2")]
+    [InlineData("Nested.Nested2.DeepNestedKey", "DeepNestedValue")]
+    public void Should_Output_Correct_Value_By_Path(string path, string expected)
+    {
+        var dictionary = CreateDictionary();
+        dictionary.GetValueByPath(path).Should().Be(expected);
+    }
+
+    private Dictionary<object, object> CreateDictionary()
+    {
+        return new Dictionary<object, object>()
+        {
+            ["Key1"] = "Value1",
+            ["Key2"] = "Value2",
+            ["Nested"] = new Dictionary<object, object>()
+            {
+                ["NestedKey1"] = "NestedValue1",
+                ["NestedKey2"] = "NestedValue2",
+                ["Nested2"] = new Dictionary<object, object>()
+                {
+                    ["DeepNestedKey"] = "DeepNestedValue"
+                }
+            },
+            ["ArrayKey"] = new Dictionary<object, object>[]
+            {
+                new()
+                {
+                    ["NestedArrayKey1"] = "NestedArrayValue1",
+                    ["NestedArrayKey2"] = "NestedArrayValue2",
+                    ["Nested"] = new Dictionary<object, object>()
+                    {
+                        ["DeepNestedArrayKey"] = "DeepNestedArrayValue"
+                    }
+                }
+            }
+        };
+    }
+}
---
+++ test/JsonValidatorForConfigMap.Test/JsonSchemaReaderTest.cs
@@ -0,0 +1,67 @@
+﻿using FluentAssertions;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace JsonValidatorForConfigMap.Test;
+
+public class JsonSchemaReaderTest
+{
+    [Fact]
+    public async Task Should_Throw_If_Schema_File_Not_Exist()
+    {
+        var reader = new JsonSchemaReader(new NullLogger<JsonSchemaReader>());
+        await reader
+            .Awaiting(r => r.ReadSchemaFromFile("non-existing"))
+            .Should()
+            .ThrowAsync<Exception>()
+            .WithMessage("File not found:*");
+    }
+
+    [Fact]
+    public async Task Should_Throw_If_Sample_Json_File_Not_Exist()
+    {
+        var reader = new JsonSchemaReader(new NullLogger<JsonSchemaReader>());
+        await reader
+            .Awaiting(r => r.ReadSchemaFromSampleJson("non-existing"))
+            .Should()
+            .ThrowAsync<Exception>()
+            .WithMessage("File not found:*");
+    }
+
+    [Fact]
+    public async Task Should_Toggle_Every_Property_To_Required_When_Reading_Sample_Json()
+    {
+        var reader = new JsonSchemaReader(new NullLogger<JsonSchemaReader>());
+        var schema = await reader.ReadSchemaFromSampleJson("JsonSchemaReaderTest.SampleJson.json");
+
+        schema.RequiredProperties
+            .Should()
+            .HaveCount(3)
+            .And.ContainInOrder("ExampleString", "Nested", "Array");
+
+        schema.ActualProperties["Nested"].ActualSchema.RequiredProperties
+            .Should()
+            .HaveCount(2)
+            .And.ContainInOrder("NestedInt", "NestedBool");
+
+        schema.Definitions["Array"].ActualSchema.RequiredProperties
+            .Should()
+            .HaveCount(1)
+            .And.ContainInOrder("ArrayObjString");
+    }
+
+    [Fact]
+    public async Task Should_Forbid_Additional_Properties_In_All_Definitions_When_Reading_Sample_Json()
+    {
+        var reader = new JsonSchemaReader(new NullLogger<JsonSchemaReader>());
+        var schema = await reader.ReadSchemaFromSampleJson("JsonSchemaReaderTest.SampleJson.json");
+
+        schema.AllowAdditionalItems.Should().BeTrue();
+        schema.AllowAdditionalProperties.Should().BeFalse();
+
+        schema.ActualProperties["Nested"].ActualSchema.AllowAdditionalItems.Should().BeTrue();
+        schema.ActualProperties["Nested"].ActualSchema.AllowAdditionalProperties.Should().BeFalse();
+
+        schema.ActualProperties["Array"].ActualSchema.AllowAdditionalItems.Should().BeTrue();
+        schema.ActualProperties["Array"].ActualSchema.AllowAdditionalProperties.Should().BeFalse();
+    }
+}
